eb828ce5d48112b497cd455a885cc853534cc0ed
Aneesha Kommineni <aneeshak@Aneeshas-MBP.guest.corp.microsoft.com>
2019-08-02 13:07:16 -0700

    theming change colors

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Button text="hello world"></Button>
+      {/* <Button2>outside treat provider</Button2> */}
       <TreatProvider theme={theme}><Button2>HAII</Button2></TreatProvider>
     </div>
   );
--- src/theme.treat.js
+++ src/theme.treat.js
@@ -1,6 +1,6 @@
 import { createTheme } from 'treat';
 
 export default createTheme({
-  brandColor: 'blue',
+  brandColor: 'pink',
   grid: 4
 });
